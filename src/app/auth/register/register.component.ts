d5e384394df67a767e38113a0e46d20644180748
sajedaquraan <sajedaalquraan1@gmail.com>
2024-10-16 11:18:50 +0300

    Register and Login

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';  // Import this for reactive forms
+import { AuthService } from 'src/app/Services/auth.service';
 
 @Component({
   selector: 'app-register',
@@ -7,29 +11,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  email: string = '';
-  password: string = '';
-  confirmPassword: string = '';
-  rememberMe: boolean = true; // Initialized to true
+  constructor(private router: Router,public auth:AuthService) {}
 
-  constructor() {}
+  registerForms: FormGroup = new FormGroup({
+    FIRST_NAME: new FormControl('', [Validators.required]),
+    LAST_NAME: new FormControl('', [Validators.required]),  // Updated placeholder
+    EMAIL: new FormControl('', [Validators.required, Validators.email]),
+    // PhoneNumber: new FormControl('', [Validators.required, Validators.pattern(/^\d{10}$/)]),  // Example validation for phone number
+    PASSWORD: new FormControl('', [Validators.required, Validators.minLength(8)]),
+    REPASSWORD: new FormControl('')
+  });
 
-  ngOnInit(): void {}
-
-  onSubmit(): void {
-    if (this.rememberMe) {
-      // Save email, password, and rememberMe status in localStorage
-      localStorage.setItem('email', this.email);
-      localStorage.setItem('password', this.password);
-      localStorage.setItem('rememberMe', 'true');
-    } else {
-      // Clear stored data if 'Remember me' is unchecked
-      localStorage.removeItem('email');
-      localStorage.removeItem('password');
-      localStorage.removeItem('rememberMe');
-      alert("You don't click on Remember me!");
-    }
-    alert('Register successfully');
-    console.log('Form submitted with:', { email: this.email, password: this.password, rememberMe: this.rememberMe });
+  ngOnInit(): void {
+    // Call matchError when passwords change
+    this.registerForms.controls['PASSWORD'].valueChanges.subscribe(() => {
+      this.matchError();
+    });
+    this.registerForms.controls['REPASSWORD'].valueChanges.subscribe(() => {
+      this.matchError();
+    });
   }
+
+  matchError() {
+    if (this.registerForms.controls['PASSWORD'].value === this.registerForms.controls['REPASSWORD'].value) 
+      {
+        console.log('no error in password match');
+      this.registerForms.controls['REPASSWORD'].setErrors(null);
+    } else {
+      console.log('error in password match');
+      this.registerForms.controls['REPASSWORD'].setErrors({ MissMatch: true });
+    }
+  }
+
+  Submit() {
+    if (this.registerForms.valid) {
+      this.auth.Register(this.registerForms.value); 
+      console.log('valid form',this.registerForms.value);
+
+    } else {
+      console.log('Form is invalid!');
+    }
+  }
+
 }
