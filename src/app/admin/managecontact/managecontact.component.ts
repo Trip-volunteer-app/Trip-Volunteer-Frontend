74e2965227e2480a1cb7f38f82b1ddc263d1e90a
Hadeel-Ali-Obaid <hadeelobaid71@gmail.com>
2024-10-29 14:21:06 +0200

    contactus functionality

--- src/app/admin/managecontact/managecontact.component.ts
+++ src/app/admin/managecontact/managecontact.component.ts
@@ -13,24 +13,39 @@ export class ManagecontactComponent implements OnInit {
 
   @ViewChild('callDeleteDialog') deleteDialog !: TemplateRef<any>;
   @ViewChild('callCreateDailog') createDialog !: TemplateRef<any>;
-  @ViewChild('callUpdateDailog') updateDialog !: TemplateRef<any>;
+  @ViewChild('callUpdateDialog') updateDialog !: TemplateRef<any>;
 
   constructor(
     public contact: ContactusService,
     public dialog: MatDialog,
   ) { }
 
-  ngOnInit(): void {
+  async ngOnInit(): Promise<void> {
+
     this.contact.getAllContactElements();
-    console.log(this.contact.getAllContactElements());
-    this.contact.getWebsiteInfo();
+    await this.contact.GetSelectedElement(); 
+    if (this.contact.selectedContactElements) {
+      this.selectedElementId = this.contact.selectedContactElements.contactus_Elements_Id;
+      console.log(this.selectedElementId);
+    } else {
+      console.error('No selected element found');
+    }
   }
 
   CreateContact: FormGroup = new FormGroup({
     image1: new FormControl('', Validators.required),
     hero_Img: new FormControl('', Validators.required),
-    header: new FormControl('', Validators.required)
+    header: new FormControl('', [Validators.required, Validators.maxLength(30)])
   });
+
+  openCreateDialog() {
+    this.dialog.open(this.createDialog)
+  }
+
+  save() {
+    this.contact.createContactUsElements(this.CreateContact.value)
+    console.log(this.CreateContact.value);
+  }
   
   openDeleteDialog(id: number) {
     console.log(id);
@@ -44,9 +59,33 @@ export class ManagecontactComponent implements OnInit {
     }
     );
   }
-  openCreateDialog() {
-    this.dialog.open(this.createDialog)
+
+  onElementSelect(id: number) {
+    this.selectedElementId = id;
+    this.contact.UpdateSelectedElement(id);
   }
+
+  UpdateContact: FormGroup = new FormGroup({
+    contactus_Elements_Id: new FormControl('', Validators.required),
+    image1: new FormControl('', Validators.required),
+    hero_Img: new FormControl('', Validators.required),
+    header: new FormControl('', [Validators.required, Validators.maxLength(30)])
+    });
+
+  pData: any = {};
+  openEditDailog(obj: any) {
+    this.pData = obj;
+    this.contact.imageStorage['image1']=this.pData.image1;
+    this.contact.imageStorage['hero_Img']=this.pData.hero_Img;
+    console.log(this.pData);
+    this.UpdateContact.controls['contactus_Elements_Id'].setValue(this.pData.contactus_Elements_Id)
+    this.dialog.open(this.updateDialog)
+  }
+
+  save2() {
+    this.contact.UpdateContact(this.UpdateContact.value)
+  }
+
   uploadImage(file: any, apiPath: string, imageNum: string) {
     console.log(file.length === 0);
     if (file.length === 0) return;
@@ -62,37 +101,5 @@ export class ManagecontactComponent implements OnInit {
   
   uploadImage1(file: any) {
     this.uploadImage(file, 'uploadImage2', 'hero_Img');
-  }
-
-  save() {
-    this.contact.createContactUsElements(this.CreateContact.value)
-    console.log(this.CreateContact.value);
-  }
-  onElementSelect(id: number) {
-    this.selectedElementId = id;
-    this.contact.UpdateSelectedElement(id);
-  }
-
-  UpdateContact: FormGroup = new FormGroup({
-    contactus_Elements_Id: new FormControl('', Validators.required),
-    image1: new FormControl('', Validators.required),
-    hero_Img: new FormControl('', Validators.required),
-    header: new FormControl('', Validators.required)
-    });
-
-  pData: any = {};
-  openEditDailog(obj: any) {
-    this.pData = obj;
-    console.log(this.pData, 'dsffffffffffffffffffffffffffffffffffffff')
-    this.contact.imageStorage['image1']=this.pData.image1;
-    this.contact.imageStorage['hero_Img']=this.pData.hero_Img;
-
-    console.log(this.pData);
-    this.UpdateContact.controls['contactus_Elements_Id'].setValue(this.pData.contactus_Elements_Id)
-    this.dialog.open(this.updateDialog)
-  }
-
-  save2() {
-    this.contact.UpdateContact(this.UpdateContact.value)
   }
 }
