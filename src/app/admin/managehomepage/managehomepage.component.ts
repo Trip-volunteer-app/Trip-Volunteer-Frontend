7f469d67d725f6ce4ca3737b50767cec66e924cb
Hadeel-Ali-Obaid <hadeelobaid71@gmail.com>
2024-11-06 11:30:22 +0200

    fix manage home elements

--- src/app/admin/managehomepage/managehomepage.component.ts
+++ src/app/admin/managehomepage/managehomepage.component.ts
@@ -3,6 +3,7 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { MatDialog } from '@angular/material/dialog';
 import { AboutService } from 'src/app/Services/about.service';
 import { HomeService } from 'src/app/Services/home.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-managehomepage',
@@ -11,6 +12,8 @@ import { HomeService } from 'src/app/Services/home.service';
 })
 export class ManagehomepageComponent implements OnInit {
   selectedElementId: number | null = null; // Initialize selected element ID
+  heroImagePreview: string | ArrayBuffer | null | undefined = null;
+  logoImagePreview: string | ArrayBuffer | null | undefined = null;
 
   @ViewChild('callDeleteDialog') deleteDialog !: TemplateRef<any>;
   @ViewChild('callCreateDialog') createDialog !: TemplateRef<any>;
@@ -34,11 +37,9 @@ export class ManagehomepageComponent implements OnInit {
 
   CreateHomeElements: FormGroup = new FormGroup({
     hero_Image: new FormControl('', Validators.required),
-    image1: new FormControl('', Validators.required),
-    image2: new FormControl('', Validators.required),
-    image3: new FormControl('', Validators.required),
+    logo_Image: new FormControl('', Validators.required),
     header: new FormControl('', Validators.required),
-    text1: new FormControl('', Validators.required),
+    text1: new FormControl(''),
     title: new FormControl('', Validators.required),
   });
 
@@ -65,18 +66,15 @@ export class ManagehomepageComponent implements OnInit {
 
   onElementSelect(id: number) {
     this.selectedElementId = id;
-    console.log('#################################', id)
     this.home.UpdateSelectedHomeElement(id);
   }
 
   UpdateHomeElements: FormGroup = new FormGroup({
     home_Page_Id: new FormControl('', Validators.required),
-    hero_Image: new FormControl('', Validators.required),
-    image1: new FormControl('', Validators.required),
-    image2: new FormControl('', Validators.required),
-    image3: new FormControl('', Validators.required),
+    hero_Image: new FormControl(''),
+    logo_Image: new FormControl(''),
     header: new FormControl('', Validators.required),
-    text1: new FormControl('', Validators.required),
+    text1: new FormControl(''),
     title: new FormControl('', Validators.required),
   });
 
@@ -84,17 +82,26 @@ export class ManagehomepageComponent implements OnInit {
   openEditDialog(obj: any) {
     this.pData = obj;
     this.home.imageStorage['hero_Image'] = this.pData.hero_Image;
-    this.home.imageStorage['image1'] = this.pData.image1;
-    this.home.imageStorage['image2'] = this.pData.image2;
-    this.home.imageStorage['image3'] = this.pData.image3;
+    this.home.imageStorage['logo_Image'] = this.pData.logo_Image;
     console.log(this.pData);
     this.UpdateHomeElements.controls['home_Page_Id'].setValue(this.pData.home_Page_Id)
     this.dialog.open(this.updateDialog)
   }
 
   save2() {
-    console.log('lololololololo',this.UpdateHomeElements.value);
-    this.home.UpdateHopmePageElements(this.UpdateHomeElements.value)
+    Swal.fire({
+      title: 'Are you sure?',
+      text: 'Do you want to proceed with the booking?',
+      icon: 'warning',
+      showCancelButton: true,  // Adds a cancel button for confirmation
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Yes, book it!'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        this.home.UpdateHopmePageElements(this.UpdateHomeElements.value)
+      }
+    });
   }
 
   uploadImage(file: any, apiPath: string, imageNum: string) {
@@ -104,21 +111,40 @@ export class ManagehomepageComponent implements OnInit {
     const formData = new FormData();
     formData.append('file', fileToUpload);
     this.home.UploadAttachment(formData, apiPath, imageNum);
+    const reader = new FileReader();
+    reader.onload = (e) => {
+      if (imageNum === 'hero_Image') {
+        this.heroImagePreview = e.target?.result;
+        this.CreateHomeElements.get('hero_Image')?.markAsTouched();
+      } else if (imageNum === 'logo_Image') {
+        this.logoImagePreview = e.target?.result;
+        this.CreateHomeElements.get('logo_Image')?.markAsTouched();
+      }
+    };
+    reader.readAsDataURL(fileToUpload);
+
+    this.home.UploadAttachment(formData, apiPath, imageNum)
   }
+
 
   uploadHeroImage(file: any) {
     this.uploadImage(file, 'uploadHeroImg', 'hero_Image');
   }
 
-  uploadImage1(file: any) {
-    this.uploadImage(file, 'uploadImage1', 'image1');
+  uploadLogoImg(file: any) {
+    this.uploadImage(file, 'uploadImage1', 'logo_Image');
   }
+  clearHeroImage(): void {
+    this.heroImagePreview = null;
+    const control = this.CreateHomeElements.get('hero_Image');
+    control?.reset();
+    control?.markAsTouched();
+}
 
-  uploadImage2(file: any) {
-    this.uploadImage(file, 'uploadImage2', 'image2');
-  }
-
-  uploadImage3(file: any) {
-    this.uploadImage(file, 'uploadImage3', 'image3');
-  }
+clearLogoImage(): void {
+    this.logoImagePreview = null;
+    const control = this.CreateHomeElements.get('logo_Image');
+    control?.reset();
+    control?.markAsTouched();
+}
 }
